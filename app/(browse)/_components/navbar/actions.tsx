aed6c12a097742ad05e7a771db0890fa023bb40b
Ryan-Zhong <2864103063@qq.com>
2024-01-14 16:03:17 +0800

    update

--- app/(browse)/_components/navbar/actions.tsx
+++ app/(browse)/_components/navbar/actions.tsx
@@ -1,43 +1,34 @@
-import Link from "next/link";
-import { Clapperboard } from "lucide-react";
-import { 
-  SignInButton, 
-  UserButton, 
-  currentUser
-} from "@clerk/nextjs";
+import { SignInButton, UserButton, currentUser } from '@clerk/nextjs';
+import { Clapperboard } from 'lucide-react';
+import Link from 'next/link';
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 export const Actions = async () => {
   const user = await currentUser();
 
   return (
-    <div className="flex items-center justify-end gap-x-2 ml-4 lg:ml-0">
+    <div className='flex items-center justify-end gap-x-2 ml-4 lg:ml-0'>
       {!user && (
         <SignInButton>
-          <Button size="sm" variant="primary">
-            Login
+          <Button size='sm' variant='primary'>
+            登录
           </Button>
         </SignInButton>
       )}
       {!!user && (
-        <div className="flex items-center gap-x-4">
+        <div className='flex items-center gap-x-4'>
           <Button
-            size="sm"
-            variant="ghost"
-            className="text-muted-foreground hover:text-primary"
-            asChild
-          >
+            size='sm'
+            variant='ghost'
+            className='text-muted-foreground hover:text-primary'
+            asChild>
             <Link href={`/u/${user.username}`}>
-              <Clapperboard className="h-5 w-5 lg:mr-2" />
-              <span className="hidden lg:block">
-                Dashboard
-              </span>
+              <Clapperboard className='h-5 w-5 lg:mr-2' />
+              <span className='hidden lg:block'>我的直播</span>
             </Link>
           </Button>
-          <UserButton
-            afterSignOutUrl="/"
-          />
+          <UserButton afterSignOutUrl='/' />
         </div>
       )}
     </div>
--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -1,36 +1,24 @@
-import Link from "next/link";
-import Image from "next/image";
-import { Poppins } from "next/font/google";
+import { Poppins } from 'next/font/google';
+import Image from 'next/image';
+import Link from 'next/link';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
 const font = Poppins({
-  subsets: ["latin"],
-  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '700', '800'],
 });
 
 export const Logo = () => {
   return (
-    <Link href="/">
-      <div className="flex items-center gap-x-4 hover:opacity-75 transition">
-        <div className="bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
-          <Image
-            src="/spooky.svg"
-            alt="Gamehub"
-            height="32"
-            width="32"
-          />
+    <Link href='/'>
+      <div className='flex items-center gap-x-4 hover:opacity-75 transition'>
+        <div className='bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink'>
+          <Image src='/spooky.svg' alt='Gamehub' height='32' width='32' />
         </div>
-        <div className={cn(
-          "hidden lg:block",
-          font.className
-        )}>
-          <p className="text-lg font-semibold">
-            Gamehub
-          </p>
-          <p className="text-xs text-muted-foreground">
-            Let&apos;s play
-          </p>
+        <div className={cn('hidden lg:block', font.className)}>
+          <p className='text-lg font-semibold'>Gamehub</p>
+          <p className='text-xs text-muted-foreground'>开启直播之旅</p>
         </div>
       </div>
     </Link>
